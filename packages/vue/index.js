6eede21484f82c48c4291ffd0f77e9a88c0f743d
Thien Nguyen <tatthien.contact@gmail.com>
2021-09-03 23:02:59 +0700

    feat: add override config for vue files

--- packages/vue/index.js
+++ packages/vue/index.js
@@ -1,4 +1,13 @@
 module.exports = {
+  overrides: [
+    {
+      files: ['*.vue'],
+      parser: 'vue-eslint-parser',
+      // parserOptions: {
+      //   parser: '@typescript-eslint/parser',
+      // },
+    },
+  ],
   extends: [
     'plugin:vue/vue3-recommended',
     '@tatthien/eslint-config-ts'
