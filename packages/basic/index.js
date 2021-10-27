17a275de6e422023c841d970fd2f0231833f0291
Thien Nguyen <tatthien.contact@gmail.com>
2021-10-28 00:13:22 +0700

    feat: add more config

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -6,14 +6,23 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
+    'plugin:import/recommended',
   ],
   rules: {
+    // Import
+    'import/order': 'error',
+    'import/first': 'error',
+    'import/no-mutable-exports': 'error',
+    'import/no-unresolved': 'off',
+    'import/no-absolute-path': 'off',
+
+    // Common
     'semi': ['error', 'never'],
     'quotes': ['error', 'single'],
     'curly': ['error', 'all'],
     'comma-dangle': ['error', 'always-multiline'],
     'comma-style': ['error', 'last'],
-
     'no-unused-vars': 'warn',
+    'no-console': ['error', { allow: ['warn', 'error'] }],
   },
 }
--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -3,4 +3,8 @@ module.exports = {
     '@tatthien/eslint-config-basic',
     'plugin:@typescript-eslint/recommended',
   ],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  }
 }
